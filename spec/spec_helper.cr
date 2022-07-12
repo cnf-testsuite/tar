c2b6fb8b7d48feceba23b647dd21b00c4b14dd69
w. watson <w@vulk.co>
2022-07-12 17:00:46 -0400

    tar moved

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,15 +1,7 @@
 require "spec"
 require "colorize"
-require "../src/cnf_testsuite"
+require "../tar.cr"
 
 ENV["CRYSTAL_ENV"] = "TEST" 
 
 
-LOGGING.info "Building ./cnf-testsuite".colorize(:green)
-`crystal build --warnings none src/cnf-testsuite.cr`
-if $?.success?
-  LOGGING.info "Build Success!".colorize(:green)
-else
-  LOGGING.info "crystal build failed!".colorize(:red)
-  raise "crystal build failed in spec_helper"
-end
